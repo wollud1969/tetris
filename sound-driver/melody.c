73d2bbc7300bf97dad67c7ecfa22b8dd6de41f9f
Wolfgang Hottgenroth <wolfgang.hottgenroth@icloud.com>
2024-03-26 16:56:46 +0100

    tetris melody

--- sound-driver/melody.c
+++ sound-driver/melody.c
@@ -2,38 +2,67 @@
 #include "sequencer.h"
 
 
-/*
-const t_note notes[] = {
-  { .octave = e_O_2, .note = e_G,   .length = },
-  { .octave = e_O_2, .note = e_D,   .length = },
-  { .octave = e_O_2, .note = e_Dis, .length = },
+const t_tone notes[] = {
+  { .octave = e_O_2, .note = e_G,   .length = e_L_1_2 },
+  { .octave = e_O_2, .note = e_D,   .length = e_L_1_4 },
+  { .octave = e_O_2, .note = e_Dis, .length = e_L_1_4 },
 
-  { .octave = e_O_2, .note = e_F,   .length = },
-  { .octave = e_O_2, .note = e_Dis, .length = },
-  { .octave = e_O_2, .note = e_D,   .length = },
+  { .octave = e_O_2, .note = e_F,   .length = e_L_1_2 },
+  { .octave = e_O_2, .note = e_Dis, .length = e_L_1_4 },
+  { .octave = e_O_2, .note = e_D,   .length = e_L_1_4},
 
-  { .octave = e_O_2, .note = e_C,   .length = },
-  { .octave = e_O_2, .note = e_C,   .length = },
-  { .octave = e_O_2, .note = e_Dis, .length = },
+  { .octave = e_O_2, .note = e_C,   .length = e_L_1_2},
+  { .octave = e_O_2, .note = e_C,   .length = e_L_1_4},
+  { .octave = e_O_2, .note = e_Dis, .length = e_L_1_4},
 
-  { .octave = e_O_2, .note = e_G,   .length = },
-  { .octave = e_O_2, .note = e_F,   .length = },
-  { .octave = e_O_2, .note = e_Dis, .length = },
+  { .octave = e_O_2, .note = e_G,   .length = e_L_1_2},
+  { .octave = e_O_2, .note = e_F,   .length = e_L_1_4},
+  { .octave = e_O_2, .note = e_Dis, .length = e_L_1_4},
 
-  { .octave = e_O_2, .note = e_D,   .length = },
-  { .octave = e_O_2, .note = e_D,   .length = },
-  { .octave = e_O_2, .note = e_Dis, .length = },
+  { .octave = e_O_2, .note = e_D,   .length = e_L_1_2},
+  { .octave = e_O_2, .note = e_D,   .length = e_L_1_4},
+  { .octave = e_O_2, .note = e_Dis, .length = e_L_1_4},
 
-  { .octave = e_O_2, .note = e_F,   .length = },
-  { .octave = e_O_2, .note = e_G,   .length = },
+  { .octave = e_O_2, .note = e_F,   .length = e_L_1_2},
+  { .octave = e_O_2, .note = e_G,   .length = e_L_1_2},
 
-  { .octave = e_O_2, .note = e_Dis, .length = },
-  { .octave = e_O_2, .note = e_C,   .length = },
+  { .octave = e_O_2, .note = e_Dis, .length = e_L_1_2},
+  { .octave = e_O_2, .note = e_C,   .length = e_L_1_2},
 
-  { .octave = e_O_2, .note = e_C,   .length = },
+  { .octave = e_O_2, .note = e_C,   .length = e_L_1_2},
+
+  { .octave = e_O_2, .note = e_F,   .length = e_L_1_4},
+  { .octave = e_O_2, .note = e_F,   .length = e_L_1_4},
+  { .octave = e_O_2, .note = e_Gis, .length = e_L_1_4},
+
+  { .octave = e_O_3, .note = e_C,   .length = e_L_1_2},
+  { .octave = e_O_2, .note = e_Ais, .length = e_L_1_4},
+  { .octave = e_O_2, .note = e_Gis,   .length = e_L_1_4},
+
+  { .octave = e_O_2, .note = e_G,   .length = e_L_1_2},
+  { .octave = e_O_2, .note = e_G,   .length = e_L_1_4},
+  { .octave = e_O_2, .note = e_Dis, .length = e_L_1_4},
+
+  { .octave = e_O_2, .note = e_G,   .length = e_L_1_2},
+  { .octave = e_O_2, .note = e_F,   .length = e_L_1_4},
+  { .octave = e_O_2, .note = e_Dis, .length = e_L_1_4},
+
+  { .octave = e_O_2, .note = e_D,   .length = e_L_1_2},
+  { .octave = e_O_2, .note = e_D,   .length = e_L_1_4},
+  { .octave = e_O_2, .note = e_Dis, .length = e_L_1_4},
+
+  { .octave = e_O_2, .note = e_F,   .length = e_L_1_2},
+  { .octave = e_O_2, .note = e_G,   .length = e_L_1_2},
+
+  { .octave = e_O_2, .note = e_Dis, .length = e_L_1_2},
+  { .octave = e_O_2, .note = e_C,   .length = e_L_1_2},
+
+  { .octave = e_O_2, .note = e_C,   .length = e_L_1_2},
+
+  { .octave = e_O_2, .note = e_C,   .length = e_L_EndMark },
 };
-*/
 
+/*
 const t_tone ladder[16] = {
   { .octave = e_O_2, .note = e_C,   .length = e_L_1_4 },
   { .octave = e_O_2, .note = e_D,   .length = e_L_1_4 },
@@ -51,18 +80,19 @@ const t_tone ladder[16] = {
   { .octave = e_O_2, .note = e_D,   .length = e_L_1_4 },
   { .octave = e_O_2, .note = e_H,   .length = e_L_EndMark },
 };
+*/
 
-t_melody melodyLadder = {
+t_melody melody= {
   .idx = 0,
   .lengthCnt = 0,
-  .tones = ladder
+  .tones = notes
 };
 
 
 void melodyInit() {
   psgWrite(07, 0b11111110);
   psgWrite(010, 03);
-  sequencerPlayMelody(&melodyLadder);
+  sequencerPlayMelody(&melody);
 }
 
 
