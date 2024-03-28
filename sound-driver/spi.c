fdb524c8d41ff65040d494c6c3b259d75aaa612b
Wolfgang Hottgenroth <wolfgang.hottgenroth@icloud.com>
2024-03-28 16:53:19 +0100

    separate SPI

---
+++ sound-driver/spi.c
@@ -0,0 +1,29 @@
+#include <msp430g2553.h>
+#include "spi.h"
+
+
+void __attribute__ ((interrupt (USCIAB0RX_VECTOR))) receive() {
+  if (UC0IFG & UCB0RXIFG) {
+    // receive an octet
+  }
+}
+
+
+void spiInit() {
+  // SPI slave
+  // BIT4: UCB0STE
+  // BIT5: UCB0CLK
+  // BIT6: UCB0SOMI
+  // BIT7: UCB0SIMO
+  P1SEL |= BIT4 | BIT5 | BIT6 | BIT7;
+  P1SEL2 |= BIT4 | BIT5 | BIT6 | BIT7;
+
+  // most significant bit first, enable STE
+  UCB0CTL0 = UCSYNC | UCMSB | UCMODE_2;
+  UCB0CTL1 = 0x00;
+
+  // enable RX interrupt
+  UC0IE |= UCB0RXIE;
+}
+
+
---
+++ sound-driver/spi.h
@@ -0,0 +1,9 @@
+#ifndef _SPI_H_
+#define _SPI_H_
+
+
+void spiInit();
+
+
+
+#endif // _SPI_H_
