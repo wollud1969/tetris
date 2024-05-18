a92a3beb9629bb6187d584313de81e89f144045c
Wolfgang Hottgenroth <wolfgang.hottgenroth@icloud.com>
2024-05-18 20:38:54 +0200

    more colors

--- rgb-driver/colors.h
+++ rgb-driver/colors.h
@@ -3,6 +3,7 @@
 
 
 #define _off 0x00
+
 #define _blue 0x01
 #define _green 0x02
 #define _orange 0x03
@@ -16,8 +17,22 @@
 #define _yellow 0x0b
 #define _white 0x0c
 #define _red 0x0d
-#define _flash 0x0e
-#define _color_end 0x0f
+
+#define _dazzling_blue 0x0e
+#define _dazzling_green 0x0f
+#define _dazzling_orange 0x10
+#define _dazzling_rose 0x11
+#define _dazzling_magenta 0x12
+#define _dazzling_violet 0x13
+#define _dazzling_azure 0x14
+#define _dazzling_cyan 0x15
+#define _dazzling_springgreen 0x16
+#define _dazzling_chartreuse 0x17
+#define _dazzling_yellow 0x18
+#define _dazzling_white 0x19
+#define _dazzling_red 0x1a
+
+#define _color_end 0x1b
 
 
 #endif
